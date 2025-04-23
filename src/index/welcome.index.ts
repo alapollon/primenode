06e0bfcb635aff1df44146509c928761a13978ac
wyvernghost <65455574+alapollon@users.noreply.github.com>
2025-04-23 17:54:38 -0400

    Furthermore all commits will be mergers. This version along with prior versions will be deleted to this point

---
+++ src/index/welcome.index.ts
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Head} from '@react-ssr/nestjs-express';
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+
+interface IndexProps {
+    welcome_message: string; 
+}
+
+class captcha {
+  constructor(tminus){
+    // todo many things! check journal 
+      Tdifferential = 0
+  } 
+  hit({}){
+    //
+  } 
+  lct({}){
+    return(<div> <LoadCanvasTemplate > </div> )
+  } lctnr(){
+    
+  }
+
+
+}
+
+const poke_button = () => {
+
+}
+
+const home_anchor = () => {
+  
+}
+const subscribe_button = () => {
+
+}
+
+  
+
+export function WelcomeIndex (props: IndexProps) {
+
+    return (
+
+      <React.Fragment>
+        <Head>
+          <title> welcome </title>
+        </Head>
+        <div> 
+          <div> poke_button </div> 
+          <div> home_anchor </div> 
+          <div> subscribe_button </div> 
+        </div>
+      </React.Fragment>
+    )
+  };
+  /**
+   * 
+   * captcha object returns a captcha package and a . And 
+   * 
+   * 
+   * 
+   */
