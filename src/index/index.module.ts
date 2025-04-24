36ef947551c75ede3d6145588ed24654547b43dd
wyvernghost <65455574+alapollon@users.noreply.github.com>
2025-04-24 18:47:34 -0400

    incremental product refinement

---
+++ src/index/index.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { } from '@react-ssr-prepass'
+import { } from '@react-ssr/core'
+import { } from '@react-ssr/nestjs-express'
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+
+
+@Module({}) export class IndexModule {
+
+} 
